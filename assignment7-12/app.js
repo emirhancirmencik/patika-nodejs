50dbbecfb2cb97e59534f9703e56a122bd43e0b2
Emirhan ÇİRMENCİK <emrhncrmnck1905@gmail.com>
2022-03-18 13:40:13 +0300

    assignment 8 completed.

--- assignment7-12/app.js
+++ assignment7-12/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
+const ejs = require("ejs");
+
 const app = express();
 
-//TODO:  MIDDLEWARES
-
 //TODO: TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+//TODO:  MIDDLEWARES
+app.use(express.static("public"));
 
 //TODO: SCHEMA
 
@@ -13,8 +17,17 @@ const app = express();
 //ROUTES
 app.get("/", (res, req) => {
   const blog = { id: 1, title: "Blog Title", description: "Blog Description" };
-  req.send(blog);
+  req.render("index", blog);
 });
+
+app.get("/about", (res, req) => {
+  req.render("about");
+});
+
+app.get("/add_post", (res, req) => {
+  req.render("add_post");
+});
+
 const port = 3000;
 
 app.listen(port, () => {
